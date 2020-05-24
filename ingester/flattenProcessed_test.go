b8ca2e9e641c4fdbc7fb08979ca18116f3921b51
Tim Drysdale <timothy.d.drysdale@gmail.com>
2020-05-25 00:30:11 +0100

    Add TestFlattenProcessedStylus
    When marking by stylus, the textfields are not keyed, and we can
    only detect marks made via optical means. A test is added for a
    stylus-marked example, which is passing, showing that the optical check
    boxes are tuned ok (at least, for now!)

--- ingester/flattenProcessed_test.go
+++ ingester/flattenProcessed_test.go
@@ -152,16 +152,6 @@ func TestFlattenProcessedMarked(t *testing.T) {
 		"tf-subtotal-14-optical": markDetected,
 	}
 
-	/*
-		for page, fields := range expectedFields {
-			for k, v := range fields {
-				assert.Equal(t, v, actualFields[page][k])
-				if v != actualFields[page][k] {
-					fmt.Println(k)
-				}
-			}
-		}
-	*/
 	// checking by actual field value allows check for false positive optical marks
 	// without specifying all the null fields in the expected values - it is
 	// assumed automatically
@@ -193,6 +183,163 @@ func TestFlattenProcessedMarked(t *testing.T) {
 	if !result {
 		fmt.Println(actualPdf)
 	}
+
+	os.RemoveAll("./tmp-delete-me")
+
+}
+
+func TestFlattenProcessedStylus(t *testing.T) {
+
+	// process a marked paper with keyed entries
+	// check that keyed entries are picked up
+	mch := make(chan chmsg.MessageInfo)
+
+	logFile := "./flatten-process-testing.log"
+
+	f, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755)
+	assert.NoError(t, err)
+
+	defer f.Close()
+
+	logger := zerolog.New(f).With().Timestamp().Logger()
+
+	g, err := New("./tmp-delete-me", mch, &logger)
+
+	assert.NoError(t, err)
+
+	assert.Equal(t, "./tmp-delete-me", g.Root())
+
+	os.RemoveAll("./tmp-delete-me")
+
+	g.EnsureDirectoryStructure()
+
+	templateFiles, err := g.GetFileList("./test-fs/etc/overlay/template")
+	assert.NoError(t, err)
+
+	for _, file := range templateFiles {
+		destination := filepath.Join(g.OverlayTemplate(), filepath.Base(file))
+		err := Copy(file, destination)
+		assert.NoError(t, err)
+	}
+
+	exam := "Practice"
+	stage := "marked"
+
+	err = g.SetupExamPaths(exam)
+
+	assert.NoError(t, err)
+
+	source := "./test-flatten/Practice-B999999-maTDD-marked-stylus.pdf"
+
+	destinationDir := g.Ingest()
+
+	assert.NoError(t, err)
+
+	err = g.CopyToDir(source, destinationDir)
+
+	assert.NoError(t, err)
+
+	//destinationDir, err := g.FlattenProcessedPapersFromDir(exam, stage)
+	err = g.StageFromIngest()
+	assert.NoError(t, err)
+
+	err = g.FlattenProcessedPapers(exam, stage)
+	assert.NoError(t, err)
+
+	// pagedata check
+	flattenedPath := "tmp-delete-me/usr/exam/Practice/23-marked-flattened/Practice-B999999-maTDD-marked-stylus.pdf"
+	pdMap, err := pagedata.UnMarshalAllFromFile(flattenedPath)
+	if err != nil {
+		t.Error(err)
+	}
+
+	//parsesvg.PrettyPrintStruct(pdMap)
+
+	// check question values, and comments on page 1 in currentData
+	// check previousData has one set, with null values for comments and values
+
+	assert.Equal(t, "B999999", pdMap[1].Current.Item.Who)
+	assert.Equal(t, "further-processing", pdMap[1].Current.Process.ToDo)
+	assert.Equal(t, "ingester", pdMap[1].Current.Process.For)
+
+	numPages, err := count.Pages(flattenedPath)
+
+	assert.NoError(t, err)
+
+	actualFields := make(map[int]map[string]string)
+
+	for i := 1; i <= numPages; i++ {
+		fieldsForPage := make(map[string]string)
+
+		for _, item := range pdMap[i].Current.Data {
+			fieldsForPage[item.Key] = item.Value
+		}
+
+		actualFields[i] = fieldsForPage
+	}
+
+	expectedFields := make(map[int]map[string]string)
+
+	expectedFields[1] = map[string]string{
+		"tf-page-ok-optical":     markDetected,
+		"tf-q1-mark-optical":     markDetected,
+		"tf-q1-number-optical":   markDetected,
+		"tf-q1-section-optical":  markDetected,
+		"tf-subtotal-00-optical": markDetected,
+		"tf-subtotal-03-optical": markDetected,
+		"tf-subtotal-05-optical": markDetected,
+	}
+
+	expectedFields[2] = map[string]string{
+		"tf-page-bad-optical": markDetected,
+	}
+
+	expectedFields[3] = map[string]string{
+		"tf-page-ok-optical":     markDetected,
+		"tf-q1-mark-optical":     markDetected,
+		"tf-q1-number-optical":   markDetected,
+		"tf-q1-section-optical":  markDetected,
+		"tf-subtotal-01-optical": markDetected,
+		"tf-subtotal-03-optical": markDetected,
+		"tf-subtotal-04-optical": markDetected,
+		"tf-subtotal-06-optical": markDetected,
+		"tf-subtotal-07-optical": markDetected,
+		"tf-subtotal-10-optical": markDetected,
+		"tf-subtotal-13-optical": markDetected,
+	}
+
+	// checking by actual field value allows check for false positive optical marks
+	// without specifying all the null fields in the expected values - it is
+	// assumed automatically
+	for page, fields := range actualFields {
+		for k, v := range fields {
+			expectedValue := "" //assume MUST BE empty field if not specified
+			if _, ok := expectedFields[page][k]; ok {
+				expectedValue = expectedFields[page][k]
+			}
+			assert.Equal(t, expectedValue, v)
+			if expectedValue != v {
+				fmt.Println(k)
+			}
+
+		}
+	}
+
+	// visual check (comments, in particular, as well as flattening of typed values)
+	actualPdf := "./tmp-delete-me/usr/exam/Practice/23-marked-flattened/Practice-B999999-maTDD-marked-stylus.pdf"
+	expectedPdf := "./expected/visual/Practice-B999999-maTDD-marked-stylus.pdf"
+
+	_, err = os.Stat(actualPdf)
+	assert.NoError(t, err)
+	_, err = os.Stat(expectedPdf)
+	assert.NoError(t, err)
+	result, err := visuallyIdenticalMultiPagePDF(actualPdf, expectedPdf)
+	assert.NoError(t, err)
+	assert.True(t, result)
+	if !result {
+		fmt.Println(actualPdf)
+	}
+
 	os.RemoveAll("./tmp-delete-me")
 
 }
