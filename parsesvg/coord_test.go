99645476cc8a5e6661387d90abd2aeeddf1e2f52
Tim Drysdale <timothy.d.drysdale@gmail.com>
2020-05-24 10:28:47 +0100

    parsesvg/coord added benchmarks on box reading

--- parsesvg/coord_test.go
+++ parsesvg/coord_test.go
@@ -176,3 +176,74 @@ func TestGetImageBoxes(t *testing.T) {
 
 	//PrettyPrintStruct(boxes)
 }
+
+//BenchmarkGetImageBoxes-32    	      73	  35286670 ns/op
+// 35.286670 ms/op
+func BenchmarkGetImageBoxes(b *testing.B) {
+
+	svgLayoutPath := "./test/layout-312pt.svg"
+	spreadName := "mark"
+	widthPx := 1883
+	heightPx := 2150
+	vanilla := true
+
+	// run the function b.N times
+	for n := 0; n < b.N; n++ {
+
+		_, err := GetImageBoxesForTextFields(svgLayoutPath, spreadName, widthPx, heightPx, vanilla, -2)
+
+		assert.NoError(b, err)
+
+	}
+
+}
+
+//BenchmarkDoCheckBoxesWholePage-32    	      12	  92800839 ns/op
+// 92.800839 ms
+// for reading in the file and checking all the boxes (17 subtotal, 2 page, 6 question)
+func BenchmarkDoCheckBoxesWholePage(b *testing.B) {
+
+	svgLayoutPath := "./test/layout-312pt.svg"
+	spreadName := "mark"
+	widthPx := 1883
+	heightPx := 2150
+	vanilla := true
+	boxes, err := GetImageBoxesForTextFields(svgLayoutPath, spreadName, widthPx, heightPx, vanilla, -2)
+	assert.NoError(b, err)
+
+	// run the function b.N times
+	for n := 0; n < b.N; n++ {
+
+		_, err := optical.CheckBoxFile("./img/stylus-1.jpg", boxes)
+
+		assert.NoError(b, err)
+
+	}
+
+}
+
+// BenchmarkDoCheckBoxesOneBox-32    	      14	  79123245 ns/op
+// 79.123245 ms
+// for reading in the file and checking one box
+// Hence most of the time is in loading the file
+// So may as well optically read all the boxes on the page in one go
+func BenchmarkDoCheckBoxesOneBox(b *testing.B) {
+
+	svgLayoutPath := "./test/layout-312pt.svg"
+	spreadName := "mark"
+	widthPx := 1883
+	heightPx := 2150
+	vanilla := true
+	boxes, err := GetImageBoxesForTextFields(svgLayoutPath, spreadName, widthPx, heightPx, vanilla, -2)
+	assert.NoError(b, err)
+
+	// run the function b.N times
+	for n := 0; n < b.N; n++ {
+
+		_, err := optical.CheckBoxFile("./img/stylus-1.jpg", []optical.Box{boxes[0]})
+
+		assert.NoError(b, err)
+
+	}
+
+}
