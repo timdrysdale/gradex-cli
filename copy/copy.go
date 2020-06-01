8991407422d439f79d0e78248ad3bbc4b6342a33
Tim Drysdale <timothy.d.drysdale@gmail.com>
2020-06-01 13:50:35 +0100

    Fix copy to not be submodule

---
+++ copy/copy.go
@@ -0,0 +1,60 @@
+package copy
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+// https://github.com/mactsouk/opensource.com/blob/master/cp3.go
+// this benchmarked well for large files in the article
+
+var BUFFERSIZE int64
+
+func Copy(src, dst string, BUFFERSIZE int64, force bool) error {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		return fmt.Errorf("%s is not a regular file.", src)
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	_, err = os.Stat(dst)
+	if err == nil && !force {
+		return fmt.Errorf("File %s already exists.", dst)
+	}
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+
+	if err != nil {
+		panic(err)
+	}
+
+	buf := make([]byte, BUFFERSIZE)
+	for {
+		n, err := source.Read(buf)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if n == 0 {
+			break
+		}
+
+		if _, err := destination.Write(buf[:n]); err != nil {
+			return err
+		}
+	}
+	return err
+}
