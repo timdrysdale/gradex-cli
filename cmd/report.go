e3f969854b13212ceb356a96ccae775fcde7dc74
Tim Drysdale <timothy.d.drysdale@gmail.com>
2020-06-03 00:49:51 +0100

    Add report command for marks
    This command reads the marks out of the front cover pages. Implemented
    so far is:
    
    gradex-cli report marks-provisional <exam>
    
    This reads cover pages in 49-checker-cover, and puts in a provisional
    marks file in 99-reports.

---
+++ cmd/report.go
@@ -0,0 +1,120 @@
+/*
+Copyright © 2020 Tim Drysdale <timothy.d.drysdale@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/rs/zerolog"
+	"github.com/spf13/cobra"
+	"github.com/timdrysdale/chmsg"
+	"github.com/timdrysdale/gradex-cli/ingester"
+)
+
+// reportCmd represents the report command
+var reportCmd = &cobra.Command{
+	Use:   "report [what] [exam]",
+	Short: "produce a report of type [what]",
+	Args:  cobra.ExactArgs(2),
+	Long: `Produce a report and put in the 99-reports folder. 
+Types of report currently implemented:
+marks-provisional (csv format marks from cover sheets in 49-checker-cover)
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		what := strings.ToLower(os.Args[2])
+		exam := os.Args[3]
+
+		var s Specification
+		// load configuration from environment variables GRADEX_CLI_<var>
+		if err := envconfig.Process("gradex_cli", &s); err != nil {
+			fmt.Println("Configuration Failed")
+			os.Exit(1)
+		}
+
+		mch := make(chan chmsg.MessageInfo)
+
+		closed := make(chan struct{})
+		defer close(closed)
+		go func() {
+			for {
+				select {
+				case <-closed:
+					break
+				case msg := <-mch:
+					if s.Verbose {
+						fmt.Printf("MC:%s\n", msg.Message)
+					}
+				}
+
+			}
+		}()
+
+		logFile := filepath.Join(s.Root, "var/log/gradex-cli.log")
+		ingester.EnsureDirAll(filepath.Dir(logFile))
+		f, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0755)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		defer f.Close()
+
+		logger := zerolog.
+			New(f).
+			With().
+			Timestamp().
+			Str("command", "report").
+			Str("what", what).
+			Str("exam", exam).
+			Logger()
+
+		g, err := ingester.New(s.Root, mch, &logger)
+		if err != nil {
+			fmt.Printf("Failed getting New Ingester %v", err)
+			os.Exit(1)
+		}
+
+		// setup dirs for later when writing report
+		g.EnsureDirectoryStructure()
+
+		switch what {
+		case "marks-provisional":
+			err = g.CheckReport(exam)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(reportCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// reportCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// reportCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
